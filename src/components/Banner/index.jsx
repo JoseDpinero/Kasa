55dd9e83fc7478af783102b3b93030c78cc13890
Jose Pinero <shine.shinete@gmail.com>
2023-02-22 12:48:56 +0100

    responsive

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,14 @@ import './_banner.scss'
 function Banner({ page }) {
   return (
     <div className={'banner banner-' + page}>
-      {page === 'home' ? <p>Chez vous, partout et ailleurs</p> : null}
+      {page === 'home' ? (
+        <div className="banner__text">
+          <p>
+            Chez vous,
+            <br className="banner__text-br" /> partout et ailleurs
+          </p>
+        </div>
+      ) : null}
     </div>
   )
 }
